a53b7ecf0b4885de19d229eec9b5cdbe88f0e7f9
Alan Guerrero <guerrero_alan@gmail.com>
2018-09-20 20:41:37 -0500

    Finish insert node  at end of linked list

--- src/InsertAtTail.java
+++ src/InsertAtTail.java
@@ -1,4 +1,32 @@
+/*
+ * 
+ * Write a method to insert a node at the end of a singly-linked list. Return the head of the modified list.
+ * 
+ * Runtime Complexity 
+	O(n)
+	
+	Space Complexity 
+	O(1)
+ */
 
 public class InsertAtTail {
-
+	public ListNode insertAtTail(ListNode head, int data) {
+	    if(head == null)    return new ListNode(data);
+	    
+	    ListNode copy = head;
+	    
+	    while(copy.next != null) {
+	        copy = copy.next;
+	    }
+	    
+	    copy.next = new ListNode(data);
+	    return head;
+	}
 }
+
+class ListNode {
+    int data;
+    ListNode next;        
+    ListNode(int data) { this.data = data; }
+ }  
+
